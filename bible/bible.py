e5766eda202cedcc8f2859c163b31686a774b0f5
Anvil SwordOfTruth <anvil.swordoftruth@protonmail.com>
2023-06-25 23:06:42 -0400

    debug

--- bible/bible.py
+++ bible/bible.py
@@ -114,7 +114,7 @@ class Bible(commands.Cog):
                     chapterNumber = str(chapter["chapter"])
                 if 'contents' in chapter:
                     usfmFormat = True
-                    description += json.dumps(chapter.get("contents")) + "\n"
+                    description += json.dumps(chapter.get("contents")[0]) + "\n"
                     verses = chapter.get("contents")[3:3]
                     #description += verses[0] + "\n"
                     chapterNumber = chapter.get("chapterNumber")
